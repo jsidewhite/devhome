1b29a311519696a63d0497d162d3299a1b95fb45
Jeff Whiteside <jwhites@microsoft.com>
2023-12-12 09:49:12 -0800

    experiment starting
    experiment starting
    
    experiment starting
    
    experiment starting
    
    experiment starting
    
    experiment starting
    
    tenable for now
    
    button clickable
    
    blank dll dqm api
    
    i dunno... stuff
    
    layers upon layers of abstraction
    
    sculpting yo
    
    sculpting yo
    
    sculpting yo
    
    some crud
    
    some crud
    
    ..builds? wtf
    
    ..builds in existing Exp proj now?
    
    nothing works. deleting stuff
    
    i hate everything
    
    i hate everything
    
    i hate everything
    
    i hate everything
    
    i hate everything
    
    i hate everything
    
    i hate everything
    
    i hate everything
    
    QuietBackgroundProcesses.ElevatedServer
    
    .
    
    .
    
    .
    
    proj build dupe errors
    
    proj build dupe errors
    
    PROJECTION BUILDS?!
    
    ALMOST BUILDS
    
    deleted garbage
    
    building
    
    building
    
    toggle and 2 way binding
    
    timer text showing False/True
    
    timer text showing False/True
    
    timer text showing False/True
    
    timer counting down from 2 hrs!
    
    server retrofitted to have timer in c++!
    
    timer starting
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    changing nam,e to manager
    
    changing nam,e to manager
    
    changing nam,e to manager
    
    changing nam,e to manager
    
    changing nam,e to manager
    
    changing nam,e to manager
    
    changing nam,e to manager
    
    changing nam,e to manager
    
    WORKIKNG AFTER BIG RENAME
    
    FLAILING WITH OUTDIR
    
    being copied here - C:\h\repo\devhome\src\bin\x64\Release\net6.0-windows10.0.22000.0\AppX\bin\x64\Release\net6.0-windows10.0.22000.0\DevHome.QuietBackgroundProcesses.ElevatedServer.exe
    
    console exe copied
    
    FINALLY - 1 day to copy the exe to the appx folder
    
    ntdll name
    
    sdfksdfl

---
+++ tools/Experiments/src/ViewModels/QuietBackgroundProcessesViewModel.cs
@@ -0,0 +1,139 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using DevHome.Common.Helpers;
+using Microsoft.UI.Xaml;
+using Windows.UI.Xaml;
+
+namespace DevHome.Experiments.ViewModels;
+public class QuietBackgroundProcessesViewModel : INotifyPropertyChanged
+{
+    public QuietBackgroundProcessesViewModel()
+    {
+        // Resume countdown if there's an existing quiet window
+        if (DevHome.QuietBackgroundProcesses.QuietBackgroundProcessesManager.IsActive)
+        {
+            var timeLeftInSeconds = DevHome.QuietBackgroundProcesses.QuietBackgroundProcessesManager.TimeLeftInSeconds;
+            StartCountdownTimer(timeLeftInSeconds);
+        }
+    }
+
+    private void StartCountdownTimer(long timeLeftInSeconds)
+    {
+        DispatcherTimer_StartCountdown(timeLeftInSeconds);
+    }
+
+    private bool _isToggleOn;
+
+    public bool IsToggleOn
+    {
+        get => _isToggleOn;
+
+        set
+        {
+            if (_isToggleOn != value)
+            {
+                _isToggleOn = value;
+
+                // Stop any existing timer
+                _dispatcherTimer?.Stop();
+
+                if (_isToggleOn)
+                {
+                    TimeLeft = "Starting...";
+                    var timeLeftInSeconds = DevHome.QuietBackgroundProcesses.QuietBackgroundProcessesManager.Start();
+                    StartCountdownTimer(timeLeftInSeconds);
+                }
+                else
+                {
+                    DevHome.QuietBackgroundProcesses.QuietBackgroundProcessesManager.Stop();
+                    TimeLeft = "Session ended";
+                }
+
+                OnPropertyChanged(nameof(IsToggleOn));
+            }
+        }
+    }
+
+    private DispatcherTimer _dispatcherTimer;
+    private TimeSpan _secondsLeft;
+
+    public void DispatcherTimer_StartCountdown(long timeLeftInSeconds)
+    {
+        _dispatcherTimer = new DispatcherTimer();
+        _dispatcherTimer.Tick += DispatcherTimer_Tick;
+        _dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+        _secondsLeft = new TimeSpan(0, 0, (int)timeLeftInSeconds);
+        _dispatcherTimer.Start();
+    }
+
+    private void DispatcherTimer_Tick(object sender, object e)
+    {
+        var sessionEnded = false;
+        var zero = new TimeSpan(0, 0, 0);
+
+        // _secondsLeft -= new TimeSpan(0, 0, 1);
+        var timeLeftInSeconds2 = DevHome.QuietBackgroundProcesses.QuietBackgroundProcessesManager.TimeLeftInSeconds;
+        _secondsLeft = new TimeSpan(0, 0, (int)timeLeftInSeconds2);
+
+        if (_secondsLeft.CompareTo(zero) <= 0)
+        {
+            // The window should be closed, but let's confirm with the server
+            if (DevHome.QuietBackgroundProcesses.QuietBackgroundProcessesManager.IsActive)
+            {
+                // There has been some drift
+                var timeLeftInSeconds = DevHome.QuietBackgroundProcesses.QuietBackgroundProcessesManager.TimeLeftInSeconds;
+                _secondsLeft = new TimeSpan(0, 0, (int)timeLeftInSeconds);
+            }
+            else
+            {
+                _dispatcherTimer.Stop();
+                _secondsLeft = zero;
+                IsToggleOn = false;
+                sessionEnded = true;
+            }
+        }
+
+        if (sessionEnded)
+        {
+            TimeLeft = "Session ended";
+        }
+        else
+        {
+            TimeLeft = _secondsLeft.ToString(); // CultureInfo.InvariantCulture
+        }
+    }
+
+    private string _timeLeft = string.Empty;
+
+    public string TimeLeft
+    {
+        get => _timeLeft;
+
+        set
+        {
+            _timeLeft = value;
+            OnPropertyChanged(nameof(TimeLeft));
+        }
+    }
+
+    // INotifyPropertyChanged members
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        var handler = PropertyChanged;
+        if (handler != null)
+        {
+            handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
