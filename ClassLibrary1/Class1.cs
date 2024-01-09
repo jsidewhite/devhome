c6e2b20ec54cb01c99881b44622652d0fefed14a
Jeff Whiteside <jwhites@microsoft.com>
2024-01-09 13:05:47 -0800

    ..builds? wtf

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors
+// Licensed under the MIT license.
+
+using DevHome.Experiments.QuietBackgroundProcesses;
+
+namespace ClassLibrary1;
+
+public class Class2
+{
+    private readonly DevHome.Experiments.QuietBackgroundProcesses.Oven _myX;
+
+    public Class2()
+    {
+        DevHome.Experiments.QuietBackgroundProcesses.Oven x = new DevHome.Experiments.QuietBackgroundProcesses.Oven();
+        _myX = x;
+    }
+
+    public DevHome.Experiments.QuietBackgroundProcesses.Oven GetOven()
+    {
+        return _myX;
+    }
+}
--- tools/QuietBackgroundProcesses/QuietBackgroundProcessesCsharpApi/QuietBackgroundProcessesCsharpApi.cs
+++ tools/QuietBackgroundProcesses/QuietBackgroundProcessesCsharpApi/QuietBackgroundProcessesCsharpApi.cs
@@ -6,19 +6,10 @@ namespace DevHome.Experiments.QuietBackgroundProcesses;
 public class QuietBackgroundProcessesCsharpApi
 {
     private static bool _quietBackgroundProcessesEnabled;
-    private static int _myint;
 
     public static bool QuietBackgroundProcessesEnabled
     {
-        get
-        {
-            return _quietBackgroundProcessesEnabled;
-        }
-
-        set
-        {
-            _myint = PInvokeApiPublic.EnableQuietBackgroundProcesses();
-            _quietBackgroundProcessesEnabled = value;
-        }
+        get => _quietBackgroundProcessesEnabled;
+        set => _quietBackgroundProcessesEnabled = value;
     }
 }
