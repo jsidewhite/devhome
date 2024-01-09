6a8551bb2a4f8fb10fe8575cf02968aa0b9953d5
Jeff Whiteside <jwhites@microsoft.com>
2024-01-09 15:44:56 -0800

    i hate everything

---
+++ z/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var x = new zruncomponent.Class();
+
+int y = x.MyProperty;
+
+Console.WriteLine("value is: " + y);
