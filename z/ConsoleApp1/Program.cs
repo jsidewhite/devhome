0a9b03331a4221e959b3c8ec289a4d7042c57920
Jeff Whiteside <jwhites@microsoft.com>
2024-01-18 16:01:15 -0800

    deleted garbage

--- z/ConsoleApp1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-var x = new zruncomponent.Class();
-
-var y = x.MyProperty;
-
-Console.WriteLine("value is: " + y);
--- z/zserver/pch.h
+++
@@ -1,8 +0,0 @@
-#include <roapi.h>
-#include <wrl.h>
-#include <winerror.h>
-#include <Windows.Foundation.h>
-#include <Windows.ApplicationModel.Core.h>
-#include <windows.System.Threading.h>
-
-//#include "C:\h\repo\devhome\z\zruncomponent\Generated Files\winrt\zruncomponent.h"
