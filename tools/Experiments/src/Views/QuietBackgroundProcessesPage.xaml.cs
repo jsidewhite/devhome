f9571c4f32c17655a854be095e907eda0c16c34d
Jeff Whiteside <jwhites@microsoft.com>
2024-01-19 12:30:32 -0800

    toggle and 2 way binding

--- tools/Experiments/src/Views/QuietBackgroundProcessesPage.xaml.cs
+++ tools/Experiments/src/Views/QuietBackgroundProcessesPage.xaml.cs
@@ -6,7 +6,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using System.Xml;
 using DevHome.Common;
+using DevHome.Common.Extensions;
+using DevHome.Common.Helpers;
+using DevHome.Common.Services;
 using DevHome.Experiments.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
@@ -27,6 +32,22 @@ public sealed partial class QuietBackgroundProcessesPage : ToolPage
 {
     public override string ShortName => "Quiet Background Processes";
 
+    private bool _isExtensionEnabled = true;
+
+    public bool IsExtensionEnabled
+    {
+        get => _isExtensionEnabled;
+
+        set
+        {
+            if (_isExtensionEnabled != value)
+            {
+                _isExtensionEnabled = value;
+                DoThings(_isExtensionEnabled);
+            }
+        }
+    }
+
     public QuietBackgroundProcessesViewModel ViewModel
     {
         get;
@@ -41,5 +62,11 @@ public sealed partial class QuietBackgroundProcessesPage : ToolPage
     private void StartSessionButton_Clicked(object sender, RoutedEventArgs e)
     {
         var x = new QuietBackgroundProcesses_ElevatedServer.Class();
+        Log.Logger()?.ReportInfo("PackageDeploymentService", $"Found package {x.MyProperty}");
+    }
+
+    private void DoThings(bool iee)
+    {
+        Log.Logger()?.ReportInfo("PackageDeploymentService", $"Found package  IEEEEEEEEEEE {iee}");
     }
 }
