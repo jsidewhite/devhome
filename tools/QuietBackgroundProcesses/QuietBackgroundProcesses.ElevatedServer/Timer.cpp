d318071ae7b3ce5d5d9397b33fb5416b6e03d47c
Jeff Whiteside <jwhites@microsoft.com>
2024-01-23 16:40:22 -0800

    .

--- tools/QuietBackgroundProcesses/QuietBackgroundProcesses.ElevatedServer/Timer.cpp
+++ tools/QuietBackgroundProcesses/QuietBackgroundProcesses.ElevatedServer/Timer.cpp
@@ -3,16 +3,22 @@
 
 std::vector<Timer> g_discardedTimers;
 std::mutex g_discardMutex;
+std::thread g_discardThread;
 
 static void CleanupDiscardedTimers()
 {
-    auto lock = std::scoped_lock(g_discardMutex);
-    for (auto& timer : g_discardedTimers)
+    std::vector<Timer> discardedTimers;
     {
-        // Destruct time window on sepearate thread because its destructor may take time to end (the std::future member is blocking)
-        auto th = std::thread([discardedTimers = std::move(g_discardedTimers)]() {});
-        th.detach();
+        auto lock = std::scoped_lock(g_discardMutex);
+        discardedTimers = std::move(g_discardedTimers);
     }
+
+    // Destruct time window on sepearate thread because its destructor may take time to end (the std::future member is blocking)
+    auto th = std::thread([discardedTimers = std::move(discardedTimers)]()
+    {
+        discardedTimers.empty(); // This will block until all timers have woken up and cancel themselves
+    });
+    th.detach();
 }
 
 void Timer::Discard(std::unique_ptr<Timer> timer)
@@ -29,6 +35,9 @@ void Timer::Discard(std::unique_ptr<Timer> timer)
         g_discardedTimers.push_back(std::move(*timer));
     }
 
-    // Clear the discard pile
-
+    // Destruct time window on sepearate thread because its destructor may take time to end (the std::future member is blocking)
+    auto th = std::thread([timer = std::move(timer)]() {
+        timer.reset();
+    });
+    th.detach();
 }
