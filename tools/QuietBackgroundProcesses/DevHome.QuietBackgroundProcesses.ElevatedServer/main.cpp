e80114c3e3c62f0092dbebd2ef4dd5f7e79b189c
Jeff Whiteside <jwhites@microsoft.com>
2024-02-08 16:47:20 -0800

    PRE start: oh my goodness.... self-elevation solution from Hart is working........... wowoowwowowowowowowowo

--- tools/QuietBackgroundProcesses/DevHome.QuietBackgroundProcesses.ElevatedServer/main.cpp
+++ tools/QuietBackgroundProcesses/DevHome.QuietBackgroundProcesses.ElevatedServer/main.cpp
@@ -17,6 +17,7 @@
 
 #include <objbase.h>
 #include <roregistrationapi.h>
+#include <shellapi.h>
 
 #include <DevHome.QuietBackgroundProcesses.QuietBackgroundProcessesManager.h>
 #include "QuietState.h"
@@ -62,29 +63,69 @@ wil::unique_ro_registration_cookie RegisterWinrtClasses(_In_ PCWSTR serverName, 
     return registrationCookie;
 }
 
+void SelfElevate(std::optional<std::wstring> const& arguments)
+{
+    wchar_t szPath[MAX_PATH];
+    if (!GetModuleFileName(NULL, szPath, ARRAYSIZE(szPath)))
+    {
+        // MessageBox(hwnd, L"Couldn't find module file name", L"Couldn't find module file name", 0);
+        THROW_LAST_ERROR();
+    }
+
+    SHELLEXECUTEINFO sei = { sizeof(sei) };
+    sei.lpVerb = L"runas";
+    sei.lpFile = szPath;
+    sei.lpParameters = arguments.value().c_str();
+    sei.hwnd = NULL;
+    sei.nShow = SW_NORMAL;
+
+    if (!ShellExecuteEx(&sei))
+    {
+        // Elevated instance launched; close this one.
+        // _exit(1);
+        //THROW_HR(E_APPLICATION_EXITING);
+
+        THROW_LAST_ERROR();
+    }
+
+    
+}
+
 //int _cdecl wmain(int argc, __in_ecount(argc) PWSTR wargv[])
 int CALLBACK wWinMain(_In_ HINSTANCE, _In_ HINSTANCE, _In_ LPWSTR wargv, _In_ int)
 try
 {
+    // -ServerName:DevHome.QuietBackgroundProcesses.ElevatedServer
     constexpr WCHAR serverNamePrefix[] = L"-ServerName:";
     if (_wcsnicmp(wargv, serverNamePrefix, wcslen(serverNamePrefix)) != 0)
     {
         return E_UNEXPECTED;
     }
+    
 
     if (!IsTokenElevated(GetCurrentProcessToken()))
     {
+
+        SelfElevate(wargv);
         //return E_ACCESSDENIED;
+        Sleep(60000);
+        return 0;
     }
+    else
+    {
+
+    }
+
+    auto serverName = std::wstring{} + (wargv + wcslen(serverNamePrefix));
 
     // To be safe, force quiet mode off to begin the proceedings in case we leaked the machine state previously
     QuietState::TurnOff();
 
-    PCWSTR serverName = wargv + wcslen(serverNamePrefix);
+    // PCWSTR serverName = wargv + wcslen(serverNamePrefix);
     auto unique_rouninitialize_call = wil::RoInitialize();
 
     // Register WinRT activatable classes
-    auto registrationCookie = RegisterWinrtClasses(serverName, [] {
+    auto registrationCookie = RegisterWinrtClasses(serverName.c_str(), [] {
         // The last instance object of the module is released
         {
             auto lock = std::unique_lock<std::mutex>(g_finishMutex);
