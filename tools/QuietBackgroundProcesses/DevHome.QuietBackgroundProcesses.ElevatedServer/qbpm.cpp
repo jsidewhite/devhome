3f2868183b95e42881df6e11c6e3859c92d6a1ed
Jeff Whiteside <jwhites@microsoft.com>
2024-02-16 11:04:29 -0800

    helpmegod

--- tools/QuietBackgroundProcesses/DevHome.QuietBackgroundProcesses.ElevatedServer/qbpm.cpp
+++ tools/QuietBackgroundProcesses/DevHome.QuietBackgroundProcesses.ElevatedServer/qbpm.cpp
@@ -8,54 +8,55 @@
 //#include <Holographic.SI.HotKeyDispatcher.h>
 //#include "HotKeys.h"
 //#include "DevHome.QuietBackgroundProcesses.QuietBackgroundProcessesSession_h.h"
-#include "DevHome.QuietBackgroundProcesses.QuietBackgroundProcessesSessionManager.h"
+//#include "DevHome.QuietBackgroundProcesses.QuietBackgroundProcessesSessionManager.h"
+#include "DevHome.QuietBackgroundProcesses.QuietBackgroundProcessesSessionManager_h.h"
 
-//namespace Windows::Internal::Shell::Holographic::Infrastructure::implementation
-//{
-
-class QuietBackgroundProcessesSessionManager :
-    public Microsoft::WRL::RuntimeClass<
-        Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::WinRt>,
-        DevHome::QuietBackgroundProcesses::IQuietBackgroundProcessesSessionManager,
-        Microsoft::WRL::FtmBase>
+namespace ABI::DevHome::QuietBackgroundProcesses
 {
-    InspectableClass(RuntimeClass_DevHome_QuietBackgroundProcesses_QuietBackgroundProcessesSessionManager, FullTrust);
-
-public:
-    STDMETHODIMP RuntimeClassInitialize() noexcept
+    class QuietBackgroundProcessesSessionManager :
+        public Microsoft::WRL::RuntimeClass<
+            Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::WinRt>,
+            ABI::DevHome::QuietBackgroundProcesses::IQuietBackgroundProcessesSessionManager,
+            Microsoft::WRL::FtmBase>
     {
-        if (IsDebuggerPresent())
-        {
-            DebugBreak();
-        }
-        return S_OK;
-    }
+        InspectableClass(RuntimeClass_DevHome_QuietBackgroundProcesses_QuietBackgroundProcessesSessionManager, FullTrust);
 
-    // IQuietBackgroundProcessesSessionManager
-    STDMETHODIMP GetInt(int* result) noexcept override
+    public:
+        STDMETHODIMP RuntimeClassInitialize() noexcept
+        {
+            if (IsDebuggerPresent())
+            {
+                DebugBreak();
+            }
+            return S_OK;
+        }
+
+        // IQuietBackgroundProcessesSessionManager
+        STDMETHODIMP GetInt(int* result) noexcept override
+        {
+            if (IsDebuggerPresent())
+            {
+                DebugBreak();
+            }
+            *result = 34;
+            return S_OK;
+        }
+    };
+    //}
+
+    /*
+    class QuietBackgroundProcessesSessionManagerStatics WrlFinal :
+        public Microsoft::WRL::AgileActivationFactory<
+            Microsoft::WRL::Implements<DevHome::QuietBackgroundProcesses::IQuietBackgroundProcessesSessionManagerStatics>>
     {
-        if (IsDebuggerPresent())
-        {
-            DebugBreak();
-        }
-        *result = 34;
-        return S_OK;
-    }
-};
-//}
+        InspectableClassStatic(RuntimeClass_DevHome_QuietBackgroundProcesses_QuietBackgroundProcessesSessionManager, FullTrust);
 
-/*
-class QuietBackgroundProcessesSessionManagerStatics WrlFinal :
-    public Microsoft::WRL::AgileActivationFactory<
-        Microsoft::WRL::Implements<DevHome::QuietBackgroundProcesses::IQuietBackgroundProcessesSessionManagerStatics>>
-{
-    InspectableClassStatic(RuntimeClass_DevHome_QuietBackgroundProcesses_QuietBackgroundProcessesSessionManager, FullTrust);
+    public:
+        // IQuietBackgroundProcessesSessionManagerStatics
+        IFACEMETHODIMP GetSession(_COM_Outptr_ DevHome::QuietBackgroundProcesses::IQuietBackgroundProcessesSession** value) noexcept override;
+        IFACEMETHODIMP TryGetSession(_COM_Outptr_opt_ DevHome::QuietBackgroundProcesses::IQuietBackgroundProcessesSession** value) noexcept override;
+    };
+    */
 
-public:
-    // IQuietBackgroundProcessesSessionManagerStatics
-    IFACEMETHODIMP GetSession(_COM_Outptr_ DevHome::QuietBackgroundProcesses::IQuietBackgroundProcessesSession** value) noexcept override;
-    IFACEMETHODIMP TryGetSession(_COM_Outptr_opt_ DevHome::QuietBackgroundProcesses::IQuietBackgroundProcessesSession** value) noexcept override;
-};
-*/
-
-ActivatableClass(QuietBackgroundProcessesSessionManager);
+    ActivatableClass(QuietBackgroundProcessesSessionManager);
+}
