bbfdc6cde1040e5137269af843b6d9019e4adde0
Jeff Whiteside <jwhites@microsoft.com>
2024-04-15 14:14:08 -0700

    telemetry
    cannot remmeber
    
    official header from https://microsoft.visualstudio.com/OS/_git/os.2020?path=/minkernel/published/internal/telemetry/open_source/TraceLoggingConfig.h&version=GBofficial/main&_a=contents
    
    finally got the telemetry inbox native package - wrong version though
    
    correct vers now
    
    moving to separate project
    
    compiles
    
    compiles with activity called from timer
    
    compiles with activity called from timer
    
    compiles with activity called from timer
    
    fsdf
    
    fsdf
    
    fsdf
    
    fsdf
    
    fsdf
    
    fsdf
    
    fsdf
    
    clean
    
    clean
    
    clean
    
    clean
    
    clean
    
    clean
    
    clean
    
    clean
    
    fix crash
    
    cleaning
    
    jigger things
    
    new name & pdt tags
    
    spacing
    
    fix crash
    
    clean
    
    clean
    
    clean eh
    
    clean
    
    clean

--- tools/QuietBackgroundProcesses/DevHome.QuietBackgroundProcesses.PerformanceRecorderEngine/PerformanceRecorderEngine.cpp
+++ tools/QuietBackgroundProcesses/DevHome.QuietBackgroundProcesses.PerformanceRecorderEngine/PerformanceRecorderEngine.cpp
@@ -331,22 +331,34 @@ ProcessPerformanceInfo MakeProcessPerformanceInfo(DWORD processId)
         auto info = ProcessPerformanceInfo{};
         info.process = nullptr;
         info.pid = processId;
+        LOG_LAST_ERROR_MSG("Failed to open process handle for PID: %d", processId);
     }
 
     auto processPathString = TryGetProcessName(process.get());
 
     auto path = std::filesystem::path(processPathString.value_or(L""));
 
-    FILETIME createTime, exitTime, kernelTime, userTime;
-    THROW_IF_WIN32_BOOL_FALSE(GetProcessTimes(process.get(), &createTime, &exitTime, &kernelTime, &userTime));
+    FILETIME createTime{}, exitTime{}, kernelTime{}, userTime{};
+    if (process)
+    {
+        THROW_IF_WIN32_BOOL_FALSE(GetProcessTimes(process.get(), &createTime, &exitTime, &kernelTime, &userTime));
+    }
 
     std::optional<std::wstring> packageFullName;
     std::optional<std::wstring> aumid;
     wil::unique_handle processToken;
-    if (OpenProcessToken(process.get(), TOKEN_QUERY, &processToken))
+    if (process)
     {
-        packageFullName = TryGetPackageFullNameFromTokenHelper(processToken.get());
-        aumid = TryGetAppUserModelIdFromTokenHelper(processToken.get());
+        if (OpenProcessToken(process.get(), TOKEN_QUERY, &processToken))
+        {
+            packageFullName = TryGetPackageFullNameFromTokenHelper(processToken.get());
+            aumid = TryGetAppUserModelIdFromTokenHelper(processToken.get());
+        }
+        else
+        {
+            PCWSTR processPath = processPathString ? processPathString.value().c_str() : L"";
+            LOG_LAST_ERROR_MSG("Failed to open process token for PID: %d (%ls)", processId, processPath);
+        }
     }
 
     auto info = ProcessPerformanceInfo{};
